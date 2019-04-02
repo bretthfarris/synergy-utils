65170490f17f0988371fe9758519587dc8246768
bretthfarris <bretthfarris@gmail.com>
2019-04-02 10:28:32 +0000

    Fixed incorrect require path for Route Helper

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
     date: require('./lib/date'),
     db: require('./lib/db'),
     logger: require('./lib/logger'),
-    requestHelper: require('./lib/request-helper')
+    routeHelper: require('./lib/route-helper')
 };
