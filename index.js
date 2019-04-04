aa1fa7fbc73027aa7b6b6684427321aedd8f95ec
bretthfarris <bretthfarris@gmail.com>
2019-04-04 05:02:58 +0000

    Added aws as a wrapper to aws-sdk.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const uuidv1 = require('uuid/v1');
+const uuidv4 = require('uuid/v4');
+
 module.exports = {
-    date: require('./lib/date'),
-    db: require('./lib/db'),
-    logger: require('./lib/logger'),
-    routeHelper: require('./lib/route-helper')
+  "aws": require('./lib/aws'),
+  "date": require('./lib/date'),
+  "db": require('./lib/db'),
+  "logger": require('./lib/logger'),
+  "routeHelper": require('./lib/route-helper'),
+  "uuidv1": function() {
+    return uuidv1();
+  },
+  "uuidv4": function() {
+    return uuidv4();
+  }
 };
--- test/date.js
+++ test/date.js
@@ -5,7 +5,7 @@ describe('Date', function() {
   describe('date.format()', function() {
     it('should format a given date into a standard format', function() {
       var date1 = "March 8, 1984";
-      var date2 = format('1984-03-08');
+      var date2 = format('1984-03-08 12:00:00');
       expect(date2).to.be.equal(date1);
     });
   });
