94656aa6b33ec112e94b750853142f08678fffba
bretthfarris <bretthfarris@gmail.com>
2019-04-05 05:56:41 +0000

    Added urlHelper.getFilename()

--- test/url-helper.js
+++ test/url-helper.js
@@ -2,8 +2,16 @@ const expect = require('chai').expect;
 const urlHelper = require('../lib/url-helper');
 
 describe('URL Helper', function() {
+  describe('urlHelper.getFilename()', function() {
+    it('should return the filename from the url', function(done) {
+      var testUrl = 'http://www.domain.com/filename.txt';
+      var filename = urlHelper.getFilename(testUrl);
+      expect(filename).to.be.eql('filename.txt');
+      done();
+    });
+  });
   describe('urlHelper.getSubdomain()', function() {
-    it('should return the the subdomain `test` from the url `http://test.somedomain.com`', function(done) {
+    it('should return the subdomain from the url', function(done) {
       var testUrl = 'http://test.somedomain.com';
       var subdomain = urlHelper.getSubdomain(testUrl);
       expect(subdomain).to.be.eql('test');
